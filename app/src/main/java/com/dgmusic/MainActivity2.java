60ce62dd88b484498802f25c3091aa63d5dea3ee
srtcdmry <74022745+srtcdmry@users.noreply.github.com>
2021-09-21 17:09:09 +0300

    ASD

--- app/src/main/java/com/dgmusic/MainActivity2.java
+++ app/src/main/java/com/dgmusic/MainActivity2.java
@@ -2,13 +2,15 @@ package com.dgmusic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity2 extends AppCompatActivity {
-    private TextView textView1;
+    private TextView textView1,textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,12 @@ public class MainActivity2 extends AppCompatActivity {
                 +"\nAge : "+String.valueOf(getIntent().getIntExtra("age",0)+"\nCompany Name: : " + getIntent().getStringExtra("comName"));
 
         textView1.setText(gelenMesaj);
+
+        Intent i = getIntent();
+         User user = (User) i.getSerializableExtra("nesne");
+         String nest = user.getFirsName();
+        Log.e("İSİM : ",user.getComName());
+        System.out.println(nest);
 
 
     }
